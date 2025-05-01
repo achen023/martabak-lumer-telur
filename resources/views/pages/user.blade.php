ae7701b73f19afc88b5fff50dd2418e0ca8d6683
AldiSiagian53 cmsformartabak <AldiSiagian53@github.com>
2025-05-01 19:56:25 +0700

    commit

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-@extends('layout.main')
-
-@section('title', 'User Account')
-
-@section('content')
-
-@include('components.tabel-user')
-=======
 @extends('layout.layoutdashboard')
 
 @section('title', 'Dashboard Admin')
 
 @section('content')
     @include('components.table-user')
->>>>>>> a8fa68cc9177898ce746a07952e7b3011348b57d
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/produk', [ProdukController::class, 'index'])->name('produk.index');
+Route::post('/produk', [ProdukController::class, 'store'])->name('produk.store');
