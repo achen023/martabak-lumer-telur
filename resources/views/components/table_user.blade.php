a82043a77d01a3e0be5faea9bc3ef1ddcb10321f
Ferdtzy <baihaquferdi@gamil.com>
2025-06-16 20:32:22 +0700

    tabel user update

--- resources/views/components/table_user.blade.php
+++ resources/views/components/table_user.blade.php
@@ -1,5 +1,3 @@
-
-
 <div class="relative overflow-x-auto">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
@@ -7,77 +5,25 @@
         </caption>
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3">
-                    NO
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    ID
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Nama
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Email
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Password
-                </th>
-                
+                <th scope="col" class="px-6 py-3">NO</th>
+                <th scope="col" class="px-6 py-3">ID</th>
+                <th scope="col" class="px-6 py-3">Nama</th>
+                <th scope="col" class="px-6 py-3">Email</th>
+                <th scope="col" class="px-6 py-3">Password</th>
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    1
-                </th>
-                <td class="px-6 py-4">
-                    001
-                </td>
-                <td class="px-6 py-4">
-                    Bambang
-                </td>
-                <td class="px-6 py-4">
-                    bambang@gmail.com
-                </td>
-                <td class="px-6 py-4">
-                    bambang123
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    2
-                </th>
-                <td class="px-6 py-4">
-                    002
-                </td>
-                <td class="px-6 py-4">
-                    akim
-                </td>
-                </td>
-                <td class="px-6 py-4">
-                    akim123@gmail.com
-                </td>
-                <td class="px-6 py-4">
-                    akimganteng
-                </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    3
-                </th>
-                <td class="px-6 py-4">
-                    003
-                </td>
-                <td class="px-6 py-4">
-                    windah 
-                </td>
-                <td class="px-6 py-4">
-                    windah455@gmail.com
-                </td>
-                <td class="px-6 py-4">
-                    windah123
-                </td>
-            </tr>
+            @foreach ($users as $index => $user)
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $index + 1 }}
+                    </th>
+                    <td class="px-6 py-4">{{ $user->id }}</td>
+                    <td class="px-6 py-4">{{ $user->name }}</td>
+                    <td class="px-6 py-4">{{ $user->email }}</td>
+                    <td class="px-6 py-4">****</td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
